541547f7571db4349162012c94a722ef661cb112
Lili Wilson <lilimichalwilson@hunterschools.org>
2021-09-08 22:55:31 -0400

    Initial Commit

---
+++ RideTogether PublicVersion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RideTogether PublicVersion
+//
+//  Created by lili wilson on 9/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
